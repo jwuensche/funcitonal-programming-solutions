df5fcbe5c56533b5e046566ca432e1cf564c1b28
Johannes Wünsche <johannes.wuensche97@gmail.com>
2019-11-26 14:53:32 +0100

    Add text justify

--- task6/text_formatting.hs
+++ task6/text_formatting.hs
@@ -1,26 +1,56 @@
 module Main where
 
-data Format = Flushleft | Flushright | Centre | Justify deriving (Eq)
+data Format = Flushleft | Flushright | Center | Justify deriving (Eq)
 
-member_length :: [String] -> Int
-member_length = foldl (\acc x -> acc + length x) 0
+lorem = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
 
 justify :: Format -> Int -> String -> String
-justify format line_length = unlines . pad_to_space format line_length . reverse . map unwords . foldl (\acc x -> case () of _
-                                                                                                                              | acc == [] -> [[x]]
-                                                                                                                              | (member_length $ head acc) < (line_length - (length x)) -> ((head acc) ++ [x]):(tail acc)
-                                                                                                                              | otherwise -> [x]:acc
-                                                                                                      ) [] . words
+justify format width =
+  let
+    member_length :: [String] -> Int
+    member_length words = length words + (sum . map length) words
 
+    group_until_length :: Int -> [[String]] -> String -> [[String]]
+    group_until_length width acc x
+      | acc == [] = [[x]]
+      | (member_length (last acc)) <= (width - (length x)) = (init acc) ++ [(last acc) ++ [x]]
+      | otherwise = acc ++ [[x]]
 
+    block :: [String] -> Int -> [String]
+    block ws chars=
+      let
+        wc = length ws - 1
+        missing = width - chars
+      in
+        let pad_ws = (map
+                      (
+                        \(i, w) ->
+                          if i < missing && i < wc
+                          then w ++ " "
+                          else w
+                      ) . zip [0..]
+                     ) ws
+        in
+          if (member_length pad_ws - 1) == width
+          then pad_ws
+          else block pad_ws (member_length pad_ws - 1)
 
-pad_to_space :: Format -> Int -> [String] -> [String]
-pad_to_space format max_space = map (\x -> case () of _
-                                                        | format == Flushright -> (replicate (max_space - (length x)) ' ') ++ x
-                                                        | format == Centre -> replicate ((max_space - (length x)) `quot` 2) ' ' ++ x ++ replicate ((max_space - (length x)) `quot` 2) ' '
-                                                        | format == Flushleft -> x
-                                    )
+    pad :: Format -> Int -> [String] -> [String]
+    pad Flushright width string = (map (\x -> (replicate (width - (length x)) ' ') ++ x)) string
+    pad Center width string = (map (\x -> (replicate ((width - (length x)) `quot` 2) ' ') ++ x)) string
+    pad Flushleft _ string = string
+    pad Justify width string = map (\line -> unwords $ block (words line) (length line)) string
+  in
+    unlines .
+    pad format width .
+    map unwords .
+    foldl (group_until_length width) [] .
+    words
 
-main :: IO()
+main :: IO ()
 main = do
-  print "foo"
+  putStr $ justify Flushleft 5 "ab cd edfghij klm"
+  putStr $ justify Flushleft 40 lorem
+  putStr $ justify Flushright 40 lorem
+  putStr $ justify Center 40 lorem
+  putStr $ justify Justify 40 lorem
