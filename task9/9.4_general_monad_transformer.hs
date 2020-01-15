5fc1e491dbe11a5fb84cba58525154212f6fc6f6
Johannes Wünsche <johannes.wuensche97@gmail.com>
2020-01-15 16:26:43 +0100

    Complete 9.4

--- task9/9.4_general_monad_transformer.hs
+++ task9/9.4_general_monad_transformer.hs
@@ -1,23 +1,47 @@
 module Main where
-
 import Control.Monad.Trans.Class
+import Control.Monad (ap)
+data MyEither a b = Left a | Right b
+
+instance Monad (MyEither e) where
+        return = Main.Right
+        Main.Right m >>= k = k m
+        Main.Left e  >>= _ = Main.Left e
+
+instance Applicative (MyEither e) where
+    pure = Main.Right
+    a <*> b = do x <- a; y <- b; return (x y)
+
+instance Functor (MyEither e) where
+    fmap _ (Main.Left a) = Main.Left a
+    fmap f (Main.Right a) = Main.Right (f a)
+
+instance (Monad m) => Applicative (EitherTransformer a m) where
+    pure = return
+    (<*>) = ap
 
 newtype EitherTransformer a m b=
-  EitherTransformer { runEitherTransformer :: m (Either a b) }
+  EitherTransformer { runEitherTransformer :: m (MyEither a b) }
+
+instance (Monad m) => Functor (EitherTransformer a m) where
+    fmap f (EitherTransformer monad) = EitherTransformer $ monad >>= apply 
+        where 
+            apply (Main.Left s) = return $ Main.Left s
+            apply (Main.Right a) = return $ Main.Right $ f a
 
 instance (Monad m) => Monad (EitherTransformer a m) where
   -- Immplement typeclass functions.
-  return dat = EitherTransformer (return $ Right dat)
+  return dat = EitherTransformer (return $ Main.Right dat)
   ---
   (EitherTransformer monad) >>= f = EitherTransformer $ monad >>= apply where
-    apply (Right dat) = runEitherTransformer $ f dat
-    apply (Left err) = return $ Left err
+    apply (Main.Right dat) = runEitherTransformer $ f dat
+    apply (Main.Left err) = return $ Main.Left err
   ---
 
 instance MonadTrans (EitherTransformer a) where
   -- Implement typeclass functions.
   lift monad = EitherTransformer $ monad >>= may where
-    may dat = return $ Right dat
+    may dat = return $ Main.Right dat
 
 main :: IO()
 main = do
